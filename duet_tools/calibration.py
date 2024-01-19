aa01ffeb05e2dc851a7725fff81d59c11a31f261
ntutland <56928897+ntutland@users.noreply.github.com>
2024-01-19 14:08:41 -0500

    Add potential imports

--- duet_tools/calibration.py
+++ duet_tools/calibration.py
@@ -1,3 +1,43 @@
 """
 DUET Tools Calibration module
 """
+from __future__ import annotations
+
+# Core Imports
+from pathlib import Path
+import importlib.resources
+
+# from typing import ...
+
+# External Imports
+import numpy as np
+from scipy.io import FortranFile
+import geojson
+import landfire
+from landfire.geospatial import get_bbox_from_polygon
+import zipfile
+import rasterio as rio
+import rasterio.mask
+from rasterio.enums import Resampling
+import pandas as pd
+import re
+import warnings
+from pydantic import (
+    BaseModel,
+    NonNegativeFloat,
+    NonNegativeInt,
+    PositiveFloat,
+    PositiveInt,
+    computed_field,
+    field_validator,
+)
+
+# Internal Imports
+
+
+try:  # Python 3.9+
+    DATA_PATH = importlib.resources.files("duet_tools").joinpath("data")
+except AttributeError:  # Python 3.6-3.8
+    from pkg_resources import resource_filename
+
+    DATA_PATH = resource_filename("duet_tools", "data")
