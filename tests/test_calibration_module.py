838f0d803470e933b6790ab601ea419c54de44fc
ntutland <56928897+ntutland@users.noreply.github.com>
2024-02-01 11:55:08 -0500

    Update test_calibratioon_module.py

--- tests/test_calibration_module.py
+++ tests/test_calibration_module.py
@@ -9,17 +9,13 @@ from pathlib import Path
 
 from duet_tools.calibration import (
     DuetRun,
+    Targets,
+    FuelParameters,
+    LandfireQuery,
     import_duet,
     assign_targets,
-    fueltype_targets,
+    assign_fuel_parameters,
     calibrate,
-    _validate_target_args,
-    _do_calibration,
-    _maxmin_calibration,
-    _meansd_calibration,
-    _constant_calibration,
-    _moisture_weights_from_density,
-    _truncate_at_0,
 )
 
 from duet_tools.utils import write_array_to_dat, read_dat_to_array
@@ -47,50 +43,12 @@ class TestDuetRun:
 
 class TestAssignTargets:
     def test_assign_targets(self):
-        litter_moisture = assign_targets(0.1, method="constant")
-        litter_density = assign_targets(0.6, 0.05, method="meansd")
-        litter_depth = assign_targets(0.3, 0.01, method="maxmin")
-        # test data types
-        assert isinstance(litter_moisture, dict)
-        assert isinstance(litter_density, dict)
-        assert isinstance(litter_depth, dict)
-        # test dict structure
-        assert list(litter_moisture.keys()) == ["method", "constant"]
-        assert list(litter_moisture.values()) == ["constant", 0.1]
-        assert list(litter_density.keys()) == ["method", "mean", "sd"]
-        assert list(litter_density.values()) == ["meansd", 0.6, 0.05]
-        assert list(litter_depth.keys()) == ["method", "max", "min"]
-        assert list(litter_depth.values()) == ["maxmin", 0.3, 0.01]
-        # test kwarg instead of arg
-        with pytest.raises(TypeError):
-            litter_moisture = assign_targets(constant=0.1, method="constant")
-        # test wrong number of arguments
-        with pytest.raises(ValueError):
-            litter_moisture = assign_targets(0.1, 0.2, method="constant")
-        with pytest.raises(ValueError):
-            litter_moisture = assign_targets(0.1, method="meansd")
-        with pytest.raises(ValueError):
-            litter_moisture = assign_targets(0.1, 0.2, 0.3, method="maxmin")
-
-
-class TestFueltypeTargets:
-    def test_fueltype_targets(self):
-        litter_moisture = assign_targets(0.1, method="constant")
-        litter_density = assign_targets(0.6, 0.05, method="meansd")
-        litter_depth = assign_targets(0.3, 0.01, method="maxmin")
-        litter_targets = fueltype_targets(
-            density=litter_density, moisture=litter_moisture, depth=litter_depth
-        )
-        # test data type
-        assert isinstance(litter_targets, dict)
-        # test dict structure
-        assert list(litter_targets.keys()) == ["density", "moisture", "depth"]
-        assert list(litter_targets["density"].keys()) == ["method", "mean", "sd"]
-        # test not having targets for some parameters
-        litter_targets = fueltype_targets(density=litter_density)
-        assert len(litter_targets.keys()) == 1
-        litter_targets = fueltype_targets(density=litter_density, depth=litter_depth)
-        assert len(litter_targets.keys()) == 2
+        maxmin_targets = assign_targets(method="maxmin", max=1.0, min=0.2)
+        meansd_targets = assign_targets(method="meansd", mean=0.6, sd=0.03)
+        constant_target = assign_targets(method="constant", target=1.0)
+        assert isinstance(maxmin_targets, Targets)
+        assert isinstance(meansd_targets, Targets)
+        assert isinstance(constant_target, Targets)
 
 
 class TestCalibrate:
@@ -98,6 +56,6 @@ class TestCalibrate:
         # try 1 fueltype and 1 parameter
         duet_run = import_duet(TMP_DIR, 252, 252)
         grass_density = assign_targets(1.0, 0.3, method="maxmin")
-        grass_targets = fueltype_targets(density=grass_density)
+        grass_targets = assign_fuel_parameters(density=grass_density)
         calibrated_duet = calibrate(duet_run, grass=grass_targets)
         assert isinstance(calibrated_duet, DuetRun)
