ed40b5264b73579fd9bcb61a71d5889cfcbbbae7
ntutland <56928897+ntutland@users.noreply.github.com>
2024-02-08 15:21:26 -0500

    Update __init__.py

--- duet_tools/__init__.py
+++ duet_tools/__init__.py
@@ -2,7 +2,6 @@ from duet_tools.calibration import (
     DuetRun,
     Targets,
     FuelParameter,
-    LandfireQuery,
     import_duet,
     assign_targets,
     set_density,
@@ -10,8 +9,12 @@ from duet_tools.calibration import (
     set_height,
     set_fuel_parameter,
     calibrate,
-    query_landfire,
     assign_targets_from_sb40,
+)
+
+from duet_tools.landfire import (
+    LandfireQuery,
+    query_landfire,
 )
 
 from duet_tools.utils import (
@@ -19,11 +22,11 @@ from duet_tools.utils import (
     read_dat_to_array,
 )
 
+
 __all__ = [
     "DuetRun",
     "Targets",
     "FuelParameter",
-    "LandfireQuery",
     "import_duet",
     "assign_targets",
     "set_density",
@@ -31,8 +34,9 @@ __all__ = [
     "set_height",
     "set_fuel_parameter",
     "calibrate",
-    "query_landfire",
     "assign_targets_from_sb40",
+    "LandfireQuery",
+    "query_landfire",
     "write_array_to_dat",
     "read_dat_to_array",
 ]
